36fbc9812b567d31d899bcd5da8e3ac9d9df2740
Julien ANQUETIL <julien.anquetil@gmail.com>
2017-11-28 20:59:50 +0100

    Add SMTP

---
+++ Model/Transport.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace JulienAnquetil\M2SendinBlue\Model;
+
+class Transport extends \Zend_Mail_Transport_Smtp implements \Magento\Framework\Mail\TransportInterface
+{
+    /**
+     * @var \Magento\Framework\Mail\MessageInterface
+     */
+    protected $_message;
+
+    /**
+     * @param MessageInterface $message
+     * @param null $parameters
+     * @throws \InvalidArgumentException
+     */
+    public function __construct(
+        \Magento\Framework\Mail\MessageInterface $message,
+        \Magento\Framework\ObjectManagerInterface $objectmanager
+    )
+
+    {
+        $this->_objectManager = $objectmanager;
+        $helper = $this->_objectManager->create('JulienAnquetil\M2SendinBlue\Helper\Data');
+
+        if (!$message instanceof \Zend_Mail) {
+            throw new \InvalidArgumentException('The message should be an instance of \Zend_Mail');
+        }
+        $smtpHost= $helper->getGeneralConfig('smtp_host');
+        $smtpConf = [
+            'auth' => 'login',//auth type
+            'ssl' => 'tls',
+            'port' => $helper->getGeneralConfig('smtp_port'),
+            'username' => $helper->getGeneralConfig('smtp_login'),
+            'password' => $helper->getGeneralConfig('smtp_pass'),
+        ];
+
+        parent::__construct($smtpHost, $smtpConf);
+        $this->_message = $message;
+    }
+
+    /**
+     * Send a mail using this transport
+     * @return void
+     * @throws \Magento\Framework\Exception\MailException
+     */
+    public function sendMessage()
+    {
+        try {
+            parent::send($this->_message);
+        } catch (\Exception $e) {
+            throw new \Magento\Framework\Exception\MailException(new \Magento\Framework\Phrase($e->getMessage()), $e);
+        }
+    }
+}
